eae43d29a665debe21098f484200a553875ab39c
Ayush2222 <4826.stkabirnav@gmail.com>
2026-05-05 11:15:13 +0000

    v1.0.1

--- vector.cpp
+++ vector.cpp
@@ -1,11 +1,16 @@
 module;
+#include <iostream>
 export module Vector;
 export class Vector{
 	public:
+		Vector() : elem{nullptr}, sz{0} {}
 		Vector(int s);
+		Vector(std::initializer_list<double>);
+		~Vector(){ delete[] elem;std::cout << "\ndeleted\n";}
 		double& operator[](int i);
 		double operator[](int i) const;
 		int size() const;
+		void push_back(double);
 	private:
 		double* elem;
 		int sz;
@@ -24,6 +29,17 @@ double Vector::operator[](int i) const{
 
 int Vector::size() const {
 	return sz;
+}
+Vector::Vector(std::initializer_list<double> lst):elem{new double[lst.size()]}, sz{static_cast<int>(lst.size())}{
+	std::copy(lst.begin(), lst.end(), elem);
+}
+export Vector read(std::istream& is)
+{
+	//usage: Vector v = read(cin);
+	Vector v;
+	for(double d; is>>d;){}
+		//v.push_back(d); to be implemented
+	return v;
 }
 export bool operator==(const Vector& v1, const Vector& v2){
 	if(v1.size()!=v2.size())
