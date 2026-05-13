1ed8a1cac48a6b85d36105cd7f00c946c87065c1
Ayush2222 <4826.stkabirnav@gmail.com>
2026-05-13 14:51:54 +0000

    13may

---
+++ copy_and_move.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <array>
+using namespace std;
+template<class T>
+class Matrix{
+	array<int, 2> dim;
+	T* elem;
+public:
+	Matrix(int d1, int d2) : dim{d1, d2}, elem { new T[d1*d2]} {}
+	int size() const {return dim[0]*dim[1];}
+	
+	Matrix(const Matrix&); //copy constructor
+	Matrix& operator=(const Matrix&); // copy assignment
+	
+	Matrix(Matrix&&);  //move constructor
+	Matrix& operator=(const Matrix&&); //move assignment
+
+	~Matrix(){ delete[] elem;}
+};
+template<class T>
+Matrix<T>:: Matrix(const Matrix& m) : dim{m.dim}, elem{new T[m.size()]}{
+	uninitialized_copy(m.elem, m.elem+m.size(), elem);
+}
+
+
+int main(){
+	Matrix<int>* m = new Matrix<int>{4,6};
+	Matrix<int>* n = m;
+	delete m;
+	delete n;
+	return 0;
+}
